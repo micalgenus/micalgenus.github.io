14ca4e16b0878ed7114c03494fb0417cc29341f4
Micalgenus <micalgenus@gmail.com>
2019-02-18 21:28:18 +0900

    Update react

---
+++ react/precache-manifest.91bb7d97209f6ed80df37af3a53f0911.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "984aa98a85304485f6b0",
+    "url": "/static/css/main.bd650034.chunk.css"
+  },
+  {
+    "revision": "984aa98a85304485f6b0",
+    "url": "/static/js/main.984aa98a.chunk.js"
+  },
+  {
+    "revision": "13eeb2032981bf3107f9",
+    "url": "/static/js/1.13eeb203.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "83dcecf039ce9a43285d8d4f55bdfaa6",
+    "url": "/index.html"
+  }
+];
--- react/service-worker.js
+++ react/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react/precache-manifest.851169b93c36b6f3407d15d043890539.js"
+  "/precache-manifest.91bb7d97209f6ed80df37af3a53f0911.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
