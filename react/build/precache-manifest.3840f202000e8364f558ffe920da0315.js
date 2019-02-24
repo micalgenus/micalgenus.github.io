1c4fa5798deaff273b3ba420ab8ec690be6a306f
GyeongSu Han <micalgenus@gmail.com>
2019-02-24 09:56:05 +0000

    Updating to $(USERNAME)/$(REPO) from $(USERNAME)/portfolio@$(SHA)

---
+++ react/build/precache-manifest.3840f202000e8364f558ffe920da0315.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "63b391c42a260bd96a8f",
+    "url": "/static/css/main.460dffb2.chunk.css"
+  },
+  {
+    "revision": "63b391c42a260bd96a8f",
+    "url": "/static/js/main.63b391c4.chunk.js"
+  },
+  {
+    "revision": "b56dc4b2cc61cff503c6",
+    "url": "/static/js/1.b56dc4b2.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6baca834833d6864114f6ea42e0aca8b",
+    "url": "/index.html"
+  }
+];
