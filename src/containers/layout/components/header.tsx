afc8e8de02f816aa10a45650e6ad5ceda3f3b39a
GyeongSu Han <micalgenus@gmail.com>
2020-08-08 19:38:43 +0900

    Remove unused code

--- src/containers/layout/components/header.tsx
+++ src/containers/layout/components/header.tsx
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import { Link } from 'gatsby';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope, faPortrait } from '@fortawesome/free-solid-svg-icons';
-import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 import Avatar from '@micalgenus/gatsby-plugin-github-avatar';
+import { Link } from 'gatsby';
+import React, { Component } from 'react';
 
 interface Props {
   siteTitle: string;
